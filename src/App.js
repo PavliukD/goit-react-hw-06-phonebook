4672342ecb9f0876580c15abe784fd007f1f4d10
albatrozzz <61119509+albatrozzz@users.noreply.github.com>
2021-12-13 15:27:55 +0200

    добавлены базовые файлы проекта

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import { useState, useEffect } from "react";
+import { nanoid } from 'nanoid'
+
+import ContactForm from "./components/ContactForm/ContactForm";
+import Filter from "./components/Filter/Filter";
+import ContactList from "./components/ContactList/ContactList";
+
+const initialContacts = [
+  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+]
+
+function App(){
+
+  const [contacts, setContacts] = useState(initialContacts)
+  const [filter, setFilter] = useState('')
+  const [filteredContacts, setFilteredContacts] = useState([])
+
+  useEffect(() => {
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'))
+    if (savedContacts){
+      setContacts(savedContacts)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts))
+  }, [contacts])
+
+  useEffect(() => {
+    if (filter === ''){
+      setFilteredContacts(contacts)
+      return
+    }
+    setFilteredContacts(contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(filter.toLowerCase())
+    }))
+  }, [filter,contacts])
+
+
+  const filterInputChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const formSubmit = (user) => {
+    if (contacts.some(contact => {
+      return contact.name.toLowerCase() === user.name.toLowerCase()
+     })){
+       return alert(`${user.name} is already in cotacts`)
+     }
+
+     setContacts(prev => prev.concat({
+      id: nanoid(),
+      name: user.name,
+      number: user.number 
+     }))
+  }
+
+  const deleteContact = (event) => {
+    const contDelId = contacts.map(contact => {
+      return contact.id
+    }).indexOf(event.target.parentElement.id)
+    const editedContacts = [...contacts]
+    editedContacts.splice(contDelId, 1)
+    setContacts(editedContacts)
+  }
+
+
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit = {formSubmit}/>
+        <h2>Contacts</h2>
+        <Filter onChange={filterInputChange} value = {filter}/>
+        <ContactList contacts = {filteredContacts} onClick = {deleteContact}/>
+      </>
+    )
+  
+}
+
+export default App;
+
---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import s from './ContactForm.module.css'
+
+
+export default function ContactForm({onSubmit}){
+  const [name, setName] = useState('')
+  const [number, setNumber] = useState('')
+
+  const inputChange = (event) => {
+      switch(event.target.name){
+        case 'name':
+          setName(event.target.value)
+          break
+        case 'number':
+          setNumber(event.target.value)
+          break
+        default:
+          break
+      }
+    }
+
+    
+     const formSubmit = (event) => {
+        event.preventDefault()
+        onSubmit({
+          name,
+          number,
+        })
+      }
+
+        return (
+        <form onSubmit = {formSubmit} className ={s.form}>
+          <label>
+            <h3>Name</h3>
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              onChange = {inputChange}
+              value = {name}
+              className = {s.input}
+            />
+            <h3>Number</h3>
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              onChange = {inputChange}
+              value = {number}
+              className = {s.input}
+            />
+          </label>
+          <button type = "submit" className = {s.button}>Add contact</button>
+        </form> 
+        )
+}
